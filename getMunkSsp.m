fab1c05d1dcf665ca9b95c012b444cf42d2d70d9
Rob <30302616+rob-chen@users.noreply.github.com>
2024-08-05 22:20:25 -0400

    Add getMunkSsp.m

---
+++ getMunkSsp.m
@@ -0,0 +1,37 @@
+function ssp = getMunkSsp(depth, dsca)
+% getMunkSsp    Idealized deep water sound speed profile
+%   The Munk profile is an idealized ocean sound speed profile that illustrates many features
+%   typical of deep-water propagation.
+%
+%   Syntax:
+%     ssp = getMunkSsp()
+%     ssp = getMunkSsp(depth)
+%     ssp = getMunkSsp(depth, dsca)
+%
+%   Inputs:
+%     depth = Nx1 vector of water column depths in meters. Default is (0:100:5000)' m
+%     dsca = Depth of minimum sound speed (deep sound channel axis) in meters. Default is 1300 m
+%
+%   Output:
+%     ssp = Nx2 matrix of water column depths in meters and sound speeds in meters/second
+%
+%   Robert Chen
+%   09 May 2014
+  
+  if nargin < 1
+    depth = (0:100:5000)';
+  else
+    depth = depth(:);
+  end
+  
+  if nargin < 2 && depth(end) >= 3000
+    dsca = 1300;
+  else
+    error('You must specify the depth of minimum sound speed, dsca!');
+  end
+  
+  z = 2 * (depth - dsca) / dsca;
+  speed = 1500 * (1 + 0.00737*(z - 1 + exp(-z)));
+  ssp = [depth, speed];
+
+end
